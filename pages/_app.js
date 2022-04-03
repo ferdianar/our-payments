52fcf8c072e0118be6516177afdf9f58d0ab0ddb
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-04-03 23:33:41 +0700

    initial commit and develop header

--- pages/_app.js
+++ pages/_app.js
@@ -12,11 +12,7 @@ function MyApp({ Component, pageProps }) {
   useEffect(() => {
     typeof document !== undefined ? require('../js/bootstrap.min.js') : null
   }, [])
-  return (
-    <React.Fragment>
-      <Component {...pageProps} />
-    </React.Fragment>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
