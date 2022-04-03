80a24fc8ffb65a27ebac8261a592f8878d13c882
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-04-03 23:47:01 +0700

    initial commit and develop header

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
         <>
             <nav className="navbar navbar-expand-lg navbar-light bg-transparent">
                 <div className="container">
-                    <a className="navbar-brand fw-bold" href="#">Paymentku</a>
+                    <a className="navbar-brand fw-bold" href="#">OurPayments</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
