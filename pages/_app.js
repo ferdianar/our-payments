68f6a8e5c24266e053035c26154a2bf4fee97a2d
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-04-03 22:55:04 +0700

    initial commit and develop header

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,11 @@ import React, { useEffect } from 'react'
 import '../styles/globals.css'
 import "../styles/css/bootstrap.css"
 
+import "bootstrap/dist/js/bootstrap.bundle"
+
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    typeof document !== undefined ? require('../js/bootstrap.min.js') : null
+    typeof document !== undefined ? require(['../js/bootstrap.min.js', '../js/popper.min.js']) : null
   }, [])
   return (
     <React.Fragment>
